48cb5c29915c5486a3cdffa07ff62a23714e8301
WevertonWe <wevwilson1@hotmail.com>
2024-09-10 10:40:18 -0300

    composer / Stripe

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4422906519a51633dc8c4d567b6e517d09523a82',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4422906519a51633dc8c4d567b6e517d09523a82',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v15.8.0',
+            'version' => '15.8.0.0',
+            'reference' => '5ed133fa45987771f80ad300be2316c05832f6a7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
